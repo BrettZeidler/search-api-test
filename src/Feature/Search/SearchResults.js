92547bedd11b7206629aabb67777b43d1b387e32
Brett Zeidler <apeacefulpanda@hotmail.com>
2021-06-12 14:26:52 -0500

    Resolved build warning

--- src/Feature/Search/SearchResults.js
+++ src/Feature/Search/SearchResults.js
@@ -29,7 +29,7 @@ function SearchResults({ searchQuery, after, before, onNextPage, onPreviousPage 
         <div>
             <div>
                 <h3 className="App-paragraph">
-                    {searchQuery == "" ? 
+                    {searchQuery === "" ? 
                         "Enter a query to search over 70 million users" : 
                         <>Found {userCount} result{userCount === 1 ? '' : 's'} for '{searchQuery}'</>
                     }
