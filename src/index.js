dc4d1fc91fcd97daafe2875ddc334238fab64a90
Brett Zeidler <apeacefulpanda@hotmail.com>
2021-06-12 18:21:40 -0500

    Attempt at grabbing a token

--- src/index.js
+++ src/index.js
@@ -3,17 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './custom.scss';
 import App from './App';
+import CryptoJS from 'crypto-js';
 import {
   	ApolloClient,
   	InMemoryCache,
   	ApolloProvider
 } from "@apollo/client";
 
+const gatherToken = () => {
+	var bytes = CryptoJS.AES.decrypt(`${process.env.REACT_APP_BEARER_TOKEN}`, 'a-key-to-use');
+	var token = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+	return token;
+}
+
 const client = new ApolloClient({
   	uri: 'https://api.github.com/graphql',
   	cache: new InMemoryCache(),
   	headers: {
-    	authorization: `bearer ${process.env.REACT_APP_BEARER_TOKEN}`
+    	authorization: `bearer ${gatherToken()}`
   	}
 });
 
